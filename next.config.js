d0302937cf3b81bede1073ae6060a6e2434ce043
Admin <dmadhusudhan98@gmail.com>
2026-09-16 15:56:59 +0530

    added

--- next.config.js
+++ next.config.js
@@ -3,16 +3,6 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.kensleyaesthetics.com' }],
-        destination: 'https://kensleyaesthetics.com/:path*',
-        permanent: true,
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
